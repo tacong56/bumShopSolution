a34f352f57affa2aa49e4bd307aa4340c956cc47
tacong56 <tacong56@gmail.com>
2020-07-18 10:44:25 +0700

    Add project files.

---
+++ bumShopSolution.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bumShopSolution.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
