d321caa615d5e1f7781dd95162dbcac13ee95341
tacong56 <tacong56@gmail.com>
2020-08-06 11:33:44 +0700

    add service for product(create, update, delete, getallpaging)

---
+++ BumShopSolution.Utilities/Exceptions/BumShopException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BumShopSolution.Utilities.Exceptions
+{
+    public class BumShopException : Exception
+    {
+        public BumShopException()
+        {
+        }
+
+        public BumShopException(string message) : base(message)
+        {
+        }
+
+        public BumShopException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
---
+++ bumShopSolution.Application/Catalog/Products/ManageProductService.cs
@@ -0,0 +1,150 @@
+﻿using bumShopSolution.Application.Catalog.Products.Dtos;
+using bumShopSolution.Application.Catalog.Products.Dtos.Manage;
+using bumShopSolution.Application.Dtos;
+using bumShopSolution.Data.EF;
+using bumShopSolution.Data.Entities;
+using BumShopSolution.Utilities.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bumShopSolution.Application.Catalog.Products
+{
+    public class ManageProductService : IManageProductService
+    {
+        private readonly BumShopDbContext _context;
+        public ManageProductService(BumShopDbContext context)
+        {
+            context = _context;
+        }
+
+        public async Task AddViewCount(int productId)
+        {
+            var product = await _context.Products.FindAsync(productId);
+            product.ViewCount += 1;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> Create(ProductCreateRequest request)
+        {
+            var product = new Product()
+            {
+                Price = request.Price,
+                OriginalPrice = request.Price,
+                Stock = request.Stock,
+                ViewCount = 0,
+                DateCreated = DateTime.Now,
+                ProductTranslations = new List<ProductTranslation>()
+                {
+                    new ProductTranslation()
+                    {
+                        Name = request.Name,
+                        Description = request.Description,
+                        Details = request.Details,
+                        SeoDescription = request.SeoDescription,
+                        SeoAlias = request.SeoAlias,
+                        SeoTitle = request.SeoTitle,
+                        LanguageId = request.LanguageId
+                    }
+                }
+            };
+
+            _context.Products.Add(product);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> Delete(int productId)
+        {
+            var product = await _context.Products.FindAsync(productId);
+            if (product == null) throw new BumShopException($"Không thể tìm được sản phẩm: {productId}");
+
+            _context.Products.Remove(product);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request)
+        {
+            //1. Select join
+            var query = from p in _context.Products
+                        join pt in _context.ProductTranslations on p.Id equals pt.ProductId
+                        join pic in _context.ProductInCategories on p.Id equals pic.ProductId
+                        join c in _context.Categories on pic.CategoryId equals c.Id
+                        select new { p, pt, pic };
+
+            //2. Filter
+            if (!string.IsNullOrEmpty(request.Keyword))
+                query = query.Where(x => x.pt.Name.Contains(request.Keyword));
+
+            if (request.CategoryIds.Count > 0)
+                query = query.Where(p => request.CategoryIds.Contains(p.pic.CategoryId));
+
+            //3. Paging
+            int totalRow = await query.CountAsync();
+
+            var data = await query.Skip((request.PageIndex - 1) * request.PageSize)
+                .Take(request.PageSize)
+                .Select(x => new ProductViewModel()
+                {
+                    Id = x.p.Id,
+                    Name = x.pt.Name,
+                    DateCreated = x.p.DateCreated,
+                    Details = x.pt.Details,
+                    LanguageId = x.pt.LanguageId,
+                    OriginalPrice = x.p.OriginalPrice,
+                    Price = x.p.Price,
+                    SeoAlias = x.pt.SeoAlias,
+                    SeoDescription = x.pt.SeoDescription,
+                    SeoTitle = x.pt.SeoTitle,
+                    Stock = x.p.Stock,
+                    ViewCount = x.p.ViewCount,
+                }).ToListAsync();
+
+            //4. Select and projection
+            var pagedResult = new PagedResult<ProductViewModel>()
+            {
+                TotalRecord = totalRow,
+                Items = data
+            };
+
+            return pagedResult;
+        }
+
+        public async Task<int> Update(ProductUpdateRequest request)
+        {
+            var product = await _context.Products.FindAsync(request.Id);
+            var productTranslations = await _context.ProductTranslations.FirstOrDefaultAsync(x => x.ProductId == request.Id
+                                        && x.LanguageId == request.LanguageId);
+            if (product == null && productTranslations == null) throw new BumShopException($"Không thể tìm được sản phẩm với id: {request.Id}");
+
+            productTranslations.Name = request.Name;
+            productTranslations.Description = request.Description;
+            productTranslations.SeoAlias = request.SeoAlias;
+            productTranslations.SeoDescription = request.SeoDescription;
+            productTranslations.SeoTitle = request.SeoTitle;
+            productTranslations.Details = request.Details;
+
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> UpdatePrice(int productId, decimal newPrice)
+        {
+            var product = await _context.Products.FindAsync(productId);
+            if (product == null) throw new BumShopException($"Không thể tìm được sản phẩm với id: {productId}");
+            product.Price = newPrice;
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateStock(int productId, int addedQuantity)
+        {
+            var product = await _context.Products.FindAsync(productId);
+            if (product == null) throw new BumShopException($"Không thể tìm được sản phẩm với id: {productId}");
+            product.Stock += addedQuantity;
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
